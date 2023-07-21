486ab2eb05947ec713dd76ad31a05977e487020d
Kevin <kevin.cuartero@gmail.com>
2023-07-20 20:18:37 -0400

    updated gateway

--- frontend/updateVisitorCount.js
+++ frontend/updateVisitorCount.js
@@ -2,7 +2,7 @@ updateVisitorCount();
 
 async function updateVisitorCount() {
   try {
-    const response = await fetch('https://kcuartero-gw-5u3b1jgg.uc.gateway.dev/count');
+    const response = await fetch('https://kcuartero-gw-cafxka52.ue.gateway.dev/count');
     const data = await response.json();
     console.log(data);
     document.getElementById('count').innerHTML = data.count;
